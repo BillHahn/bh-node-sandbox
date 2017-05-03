7c0969bf925159694447decc99eeaf570ce81bdd
Bill Hahn <BillHahnG@gmail.com>
2017-05-03 17:41:03 -0500

    BH Initial Commit

---
+++ modhttp.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+  var http = require('http');
+  var server = http.createServer(function(request, response) {
+    console.log('got a request!');
+    response.write('hi');
+    response.end();
+  });
+
+  server.listen(3000);
+};
---
+++ module1.js
@@ -0,0 +1,6 @@
+var m2 = require('./module2');
+m2();
+//var _ = require('underscore');
+//console.log(_);
+var modhttp = require('./modhttp');
+modhttp();
